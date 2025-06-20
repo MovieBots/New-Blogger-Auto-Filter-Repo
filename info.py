bacb9a32a9c317b1d6d79f679f99039676c9a55e
MovieBots <154045205+MovieBots@users.noreply.github.com>
2025-06-20 21:16:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,7 +54,7 @@ FILE_STORE_CHANNEL = [int(fsch) if id_pattern.search(fsch) else fsch for fsch in
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1002074744533').split()]
 
 # MongoDB information
-DATABASE_URI = os.environ.get("DATABASE_URI")
+DATABASE_URI = os.environ.get("")
 SECONDDB_URI = os.environ.get(None)
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
